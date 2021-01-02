94a81b945169b01863faac515ebbe6ff20707dbc
Olivier JEAN-BAPTISTE <ojeanbaptiste75@gmail.com>
2021-01-02 16:16:53 +0100

    fixed smtp server for dev

--- wp-content/themes/agencia/inc/supports.php
+++ wp-content/themes/agencia/inc/supports.php
@@ -12,14 +12,3 @@ add_filter('upload_mimes', function ($mimes){
     $mimes['svg'] = 'image/svg+xml';
     return $mimes;
 });
-/*Supported fake SMTP Server for development */
-function mailtrap($phpmailer) {
-    $phpmailer->isSMTP();
-    $phpmailer->Host = 'smtp.mailtrap.io';
-    $phpmailer->SMTPAuth = true;
-    $phpmailer->Port = 2525;
-    $phpmailer->Username = 'f4df3e1d80e5cc';
-    $phpmailer->Password = 'fade7826c782d2';
-  }
-  
-  add_action('phpmailer_init', 'mailtrap');
